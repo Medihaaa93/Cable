cbdb4d03556c6baf606a11ccec83c18a8d7dbc7a
Medihaaa93 <mediha.fejzic@stud.fh-campuswien.ac.at>
2017-06-22 19:07:50 +0200

    Anbindung (Activities)

--- Cable/app/src/main/java/com/example/mediha/cable/MainActivity.java
+++ Cable/app/src/main/java/com/example/mediha/cable/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import static android.R.attr.id;
+import static android.R.attr.scrollbarDefaultDelayBeforeFade;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,22 +16,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BerrechnungSicherungIntent();
+        BerechnungKabel();
+        ImpressumBtn();
+
+    }
+
+    public void BerrechnungSicherungIntent() { //Sicherung ist ImageView4
+        ImageView sicherung = (ImageView) findViewById(R.id.imageView4);
+        sicherung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), BerrechnungSicherung.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    public void BerechnungKabel() { //Kable ist ImageView3
+        ImageView sicherung = (ImageView) findViewById(R.id.imageView3);
+        sicherung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), KabelBerechnung.class);
+                startActivity(i);
+            }
+        });
 
     }
 
 
-
-    public void ImpressumBtn(){
+    public void ImpressumBtn() {
 
         Button btn = (Button) findViewById(R.id.button);
 
         btn.setOnClickListener(new View.OnClickListener()
 
-    {
-        @Override
-        public void onClick (View v){
-        Intent i = new Intent(v.getContext(), Impressum.class);
-        startActivity(i);
-    }
+        {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), Impressum.class);
+                startActivity(i);
+            }
         });
+    }
 }
