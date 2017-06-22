d1d7cf16e13d2b5ab53cbd9c7838f276473a6fc9
Medihaaa93 <mediha.fejzic@stud.fh-campuswien.ac.at>
2017-06-22 17:43:51 +0200

    iwas

---
+++ Cable/app/src/main/java/com/example/mediha/cable/BerrechnungSicherung.java
@@ -0,0 +1,13 @@
+package com.example.mediha.cable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BerrechnungSicherung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berrechnung_sicherung);
+    }
+}
--- Cable/app/src/main/java/com/example/mediha/cable/MainActivity.java
+++ Cable/app/src/main/java/com/example/mediha/cable/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.mediha.cable;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = new Button(findViewById(id.button));
     }
+
+
+
+    public void ImpressumBtn(){
+
+        btn.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View v){
+        Intent i = new Intent(v.getContext(), activityClass);
+        startActivity(i);
+    }
+    }
+}
+
 }
