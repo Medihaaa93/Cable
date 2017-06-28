50ceedae51704cedf3da3946dff09c6af5bef491
Medihaaa93 <mediha.fejzic@stud.fh-campuswien.ac.at>
2017-06-28 11:06:47 +0200

    iwas
    eienfach push

--- Cable/app/src/main/java/com/example/mediha/cable/BerrechnungSicherung.java
+++ Cable/app/src/main/java/com/example/mediha/cable/BerrechnungSicherung.java
@@ -1,7 +1,9 @@
 package com.example.mediha.cable;
 
+import android.print.pdf.PrintedPdfDocument;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BerrechnungSicherung extends AppCompatActivity {
 
@@ -12,5 +14,40 @@ public class BerrechnungSicherung extends AppCompatActivity {
     }
 
 
+    public void startBtn(View view) {
 
+
+
+
+
+    }
+    //************************************************************************* PDFmaker
+/*
+    public void makePDF (){
+
+        // open a new document
+        PrintedPdfDocument document = new PrintedPdfDocument(context,
+                printAttributes);
+
+        // start a page
+        Page page = document.startPage(0);
+
+        // draw something on the page
+        View content = getContentView();
+        content.draw(page.getCanvas());
+
+        // finish the page
+        document.finishPage(page);
+
+        // add more pages
+
+        // write the document content
+        document.writeTo(getOutputStream());
+
+        //close the document
+        document.close();
+
+
+
+    }***************************************************************************************/
 }
